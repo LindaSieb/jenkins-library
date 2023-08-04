c84fef6e75b027936d8e208f631aabc6ba203e47
Vyacheslav Starostin <vyacheslav.starostin@sap.com>
2023-08-04 19:20:29 +0600

    Update readPipelineEnv

--- cmd/readPipelineEnv.go
+++ cmd/readPipelineEnv.go
@@ -2,34 +2,56 @@ package cmd
 
 import (
 	"encoding/json"
-	"github.com/SAP/jenkins-library/pkg/log"
-	"github.com/SAP/jenkins-library/pkg/piperenv"
-	"github.com/spf13/cobra"
+	"fmt"
 	"os"
 	"path"
+
+	"github.com/spf13/cobra"
+
+	"github.com/SAP/jenkins-library/pkg/config"
+	"github.com/SAP/jenkins-library/pkg/log"
+	"github.com/SAP/jenkins-library/pkg/piperenv"
+	"github.com/SAP/jenkins-library/pkg/validation"
 )
 
-// ReadPipelineEnv reads the commonPipelineEnvironment from disk and outputs it as JSON
-func ReadPipelineEnv() *cobra.Command {
-	return &cobra.Command{
-		Use:   "readPipelineEnv",
-		Short: "Reads the commonPipelineEnvironment from disk and outputs it as JSON",
-		PreRun: func(cmd *cobra.Command, args []string) {
-			path, _ := os.Getwd()
-			fatalHook := &log.FatalHook{CorrelationID: GeneralConfig.CorrelationID, Path: path}
-			log.RegisterHook(fatalHook)
-		},
-
-		Run: func(cmd *cobra.Command, args []string) {
-			err := runReadPipelineEnv()
-			if err != nil {
-				log.Entry().Fatalf("error when writing reading Pipeline environment: %v", err)
-			}
-		},
-	}
+type readPipelineEnvOptions struct {
+	GithubToken string `json:"githubToken,omitempty"`
 }
 
-func runReadPipelineEnv() error {
+func addReadPipelineEnvFlags(cmd *cobra.Command, stepConfig *readPipelineEnvOptions) {
+	cmd.Flags().StringVar(&stepConfig.GithubToken, "githubToken", os.Getenv("PIPER_githubToken"), "GitHub personal access token as per https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line")
+}
+
+// ReadPipelineEnv reads the commonPipelineEnvironment from disk and outputs it as JSON
+// func ReadPipelineEnv() *cobra.Command {
+
+// 	const STEP_NAME = "readPipelineEnv"
+
+// 	metadata := readPipelineEnvMetadata()
+// 	var stepConfig readPipelineEnvOptions
+
+// 	return &cobra.Command{
+// 		Use:   "readPipelineEnv",
+// 		Short: "Reads the commonPipelineEnvironment from disk and outputs it as JSON",
+// 		PreRun: func(cmd *cobra.Command, args []string) {
+// 			path, _ := os.Getwd()
+// 			fatalHook := &log.FatalHook{CorrelationID: GeneralConfig.CorrelationID, Path: path}
+// 			log.RegisterHook(fatalHook)
+// 		},
+
+// 		Run: func(cmd *cobra.Command, args []string) {
+// 			err := runReadPipelineEnv()
+// 			if err != nil {
+// 				log.Entry().Fatalf("error when writing reading Pipeline environment: %v", err)
+// 			}
+// 		},
+// 	}
+// }
+
+func runReadPipelineEnv(config readPipelineEnvOptions) error {
+
+	fmt.Printf("===github token: %s\n===", config.GithubToken)
+
 	cpe := piperenv.CPEMap{}
 
 	err := cpe.LoadFromDisk(path.Join(GeneralConfig.EnvRootPath, "commonPipelineEnvironment"))
@@ -44,4 +66,102 @@ func runReadPipelineEnv() error {
 	}
 
 	return nil
+}
+
+// ShellExecuteCommand Step executes defined script
+func ReadPipelineEnv() *cobra.Command {
+	const STEP_NAME = "shellExecute"
+
+	metadata := readPipelineEnvMetadata()
+	var stepConfig readPipelineEnvOptions
+
+	var createReadPipelineEnvCmd = &cobra.Command{
+		Use:   STEP_NAME,
+		Short: "Step executes defined script",
+		Long:  `Step executes defined script provided in the 'sources' parameter`,
+		PreRunE: func(cmd *cobra.Command, _ []string) error {
+			log.SetStepName(STEP_NAME)
+			log.SetVerbose(GeneralConfig.Verbose)
+
+			GeneralConfig.GitHubAccessTokens = ResolveAccessTokens(GeneralConfig.GitHubTokens)
+
+			path, _ := os.Getwd()
+			fatalHook := &log.FatalHook{CorrelationID: GeneralConfig.CorrelationID, Path: path}
+			log.RegisterHook(fatalHook)
+
+			err := PrepareConfig(cmd, &metadata, STEP_NAME, &stepConfig, config.OpenPiperFile)
+			if err != nil {
+				log.SetErrorCategory(log.ErrorConfiguration)
+				return err
+			}
+			log.RegisterSecret(stepConfig.GithubToken)
+
+			validation, err := validation.New(validation.WithJSONNamesForStructFields(), validation.WithPredefinedErrorMessages())
+			if err != nil {
+				return err
+			}
+			if err = validation.ValidateStruct(stepConfig); err != nil {
+				log.SetErrorCategory(log.ErrorConfiguration)
+				return err
+			}
+
+			return nil
+		},
+		Run: func(_ *cobra.Command, _ []string) {
+			handler := func() {
+				config.RemoveVaultSecretFiles()
+			}
+			log.DeferExitHandler(handler)
+			defer handler()
+			err := runReadPipelineEnv(stepConfig)
+			if err != nil {
+				log.Entry().Fatalf("error when writing reading Pipeline environment: %v", err)
+			}
+			log.Entry().Info("SUCCESS")
+		},
+	}
+
+	addReadPipelineEnvFlags(createReadPipelineEnvCmd, &stepConfig)
+	return createReadPipelineEnvCmd
+}
+
+// retrieve step metadata
+func readPipelineEnvMetadata() config.StepData {
+	var theMetaData = config.StepData{
+		Metadata: config.StepMetadata{
+			Name:        "readPipelineEnv",
+			Aliases:     []config.Alias{},
+			Description: "Step executes defined script",
+		},
+		Spec: config.StepSpec{
+			Inputs: config.StepInputs{
+				Secrets: []config.StepSecrets{
+					{Name: "githubTokenCredentialsId", Description: "Jenkins credentials ID containing the github token.", Type: "jenkins"},
+				},
+				Parameters: []config.StepParameters{
+					{
+						Name: "githubToken",
+						ResourceRef: []config.ResourceReference{
+							{
+								Name: "githubTokenCredentialsId",
+								Type: "secret",
+							},
+
+							{
+								Name:    "githubVaultSecretName",
+								Type:    "vaultSecret",
+								Default: "github",
+							},
+						},
+						Scope:     []string{"GENERAL", "PARAMETERS", "STAGES", "STEPS"},
+						Type:      "string",
+						Mandatory: false,
+						Aliases:   []config.Alias{{Name: "access_token"}},
+						Default:   os.Getenv("PIPER_githubToken"),
+					},
+				},
+			},
+		},
+	}
+	return theMetaData
 }
