bdf8906d237297c73aee20e1ddbbb12f443be7da
Vyacheslav Starostin <vyacheslav.starostin@sap.com>
2023-08-04 23:49:47 +0600

    Change readpipelineenv

--- vars/readPipelineEnv.groovy
+++ vars/readPipelineEnv.groovy
@@ -8,9 +8,12 @@ void call(Map parameters = [:]) {
     final script = checkScript(this, parameters) ?: this
     String piperGoPath = parameters?.piperGoPath ?: './piper'
 
-    script.sh(returnStdout: false, script: "${piperGoPath} readPipelineEnv")
+    // script.sh(returnStdout: false, script: "${piperGoPath} readPipelineEnv")
     
     def output = script.sh(returnStdout: true, script: "${piperGoPath} readPipelineEnv")
+    // ***
+    echo "===cpe (output): ${output}"
+
     Map cpeMap = script.readJSON(text: output)
     script?.commonPipelineEnvironment?.setCPEMap(script, cpeMap)
 }
